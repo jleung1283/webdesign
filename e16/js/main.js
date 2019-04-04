e2fd7b70eabe81ada738124f56f25984278af722
jleung1283 <38233067+jleung1283@users.noreply.github.com>
2019-04-04 12:59:24 -0400

    exercise stuff

---
+++ e16/js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+	//$('h1').css('color', 'white');
+		$('#number5').click(function() {
+			$('#number5').css('color', 'white');
+			$('#bird-5').toggleClass('black');
+		});
+		$('#number4').click(function() {
+			$('#number4').css('color', 'goldenrod');
+		});
+	});
