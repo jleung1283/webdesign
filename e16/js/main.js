a269097843fe6256f070342efe5a8c10144f169c
jleung1283 <38233067+jleung1283@users.noreply.github.com>
2019-04-16 00:43:02 -0400

    e16

--- e16/js/main.js
+++ e16/js/main.js
@@ -1,10 +1,32 @@
 $(document).ready(function() {
-	//$('h1').css('color', 'white');
-		$('#number5').click(function() {
-			$('#number5').css('color', 'white');
-			$('#bird-5').toggleClass('black');
-		});
-		$('#number4').click(function() {
-			$('#number4').css('color', 'goldenrod');
-		});
-	});
+  // $('h1').css('color', 'white');
+  $('#number5').click(function() {
+    $('#number5').css('color', 'white');
+    $('#word-5').toggleClass('black');
+  });
+  $('#number4').click(function() {
+    $('#number4').css('color', 'goldenrod');
+	$('#word-4').toggleClass('cyan');
+  });
+  $('#number3').hover(function() {
+    $('#number3').css('color', 'black');
+	$('#word-3').toggleClass('yellow');
+  });
+  $('#number2').mouseenter(function() {
+    $('#number2').css('color', 'pink');
+	$('#word-2').toggleClass('silver');
+  });
+  $('#number1').click(function() {
+    $('#number1').css('color', 'purple');
+    $('#word-1').toggleClass('gold');
+  });
+});
+
+$('p').addClass('bigger');
+console.log('added bigger');
+$('p').click(function(){
+  $('p').css('color', 'white');
+  // added a message to the console, helpful in debugging
+  console.log('added white');
+  $('body').toggleClass('bodyBG');
+});
